9cc7e235d33cd612048aab6bcd6e85af3ef76ae5
MandePriscille <mandepriscille1@gmail.com>
2025-12-21 12:46:16 +0100

    fix: serializers

--- backend/projetAPI/product/views.py
+++ backend/projetAPI/product/views.py
@@ -6,14 +6,24 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['POST'])
 def api_view(request):
-    query = Product.objects.all().order_by('?').first()
-    data = {}
-    if query:
+    # query = Product.objects.all().order_by('?').first()
+    # serializer = {}
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response({'details':"invalid data"})
+    
+    # data = request.data
+    # if query:
         # data['name'] = query.name
         # data['content'] = query.content
         # data['price'] = query.price
         # data = model_to_dict(query, fields=('name', 'price', 'content', 'get_discount'))
-        data=ProductSerializer(query).data
-    return Response(data)
+        # data=ProductSerializer(query).data
+
+     
+    # return Response(data)
--- clientAPI/basic.py
+++ clientAPI/basic.py
@@ -9,6 +9,7 @@ import requests
 
 
 endpoint = "http://127.0.0.1:8000/product/"
-reponse = requests.get(endpoint)
-print(reponse.json())
+# reponse = requests.get(endpoint)
+reponse = requests.post(endpoint, json={'name':'citron', 'content':'cintron', 'price':300})
+print(reponse.json()) 
 print(reponse.status_code)
