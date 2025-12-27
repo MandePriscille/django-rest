1aed5c662c34aa8d2766a71d73e956c083b2b633
MandePriscille <mandepriscille1@gmail.com>
2025-12-27 13:38:50 +0100

    user serialize for create now field

--- backend/projetAPI/product/serializers.py
+++ backend/projetAPI/product/serializers.py
@@ -3,10 +3,16 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
-        fields = ('pk', 'name', 'content', 'price', 'my_discount')
-        
+        fields = ('email', 'pk', 'name', 'content', 'price', 'my_discount')
+
+    def create(self, validated_data):
+        email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        return obj
+
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
